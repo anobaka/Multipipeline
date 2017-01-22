c8e3389195fc99596505483129289259c7ce5aba
Anobaka <anobaka@anobaka.com>
2017-01-22 16:00:34 +0800

    update docs

--- src/LazyMortal.Multipipeline/IPipeline.cs
+++ src/LazyMortal.Multipipeline/IPipeline.cs
@@ -6,8 +6,22 @@ namespace LazyMortal.Multipipeline
 {
 	public interface IPipeline
 	{
+		/// <summary>
+		/// Pipeline's name
+		/// </summary>
 		string Name { get; set; }
+		/// <summary>
+		/// Whether a request belongs to this pipeline
+		/// </summary>
+		/// <param name="ctx"></param>
+		/// <returns></returns>
 		Task<bool> ResolveAsync(HttpContext ctx);
+		/// <summary>
+		/// <para>Configure this pipeline.</para>
+		/// <para>e.g. Add some specific authentications for this pipeline</para>
+		/// </summary>
+		/// <param name="app"></param>
+		/// <returns></returns>
 		Task ConfigurePipeline(IApplicationBuilder app);
 	}
 }
--- src/LazyMortal.Multipipeline/MultipipelineOptions.cs
+++ src/LazyMortal.Multipipeline/MultipipelineOptions.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 
 namespace LazyMortal.Multipipeline
 {
     public class MultipipelineOptions
     {
+		/// <summary>
+		/// Default value is "Pipeline", used to store current pipeline in <see cref="HttpContext.Items"/>
+		/// </summary>
 	    public string PipelineHttpContextItemKey { get; set; } = "Pipeline";
     }
 }
